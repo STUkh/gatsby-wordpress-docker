75021abe87b6323883a53ad4ede5c2a7d9e53fd3
Alex Stepchenkov <stukak@gmail.com>
2019-03-23 15:34:39 +0200

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+        resolve: "gatsby-source-wordpress",
+        options: {
+            baseUrl: process.env.WORDPRESS_URL,
+            protocol: "http",
+            hostingWPCOM: false,
+            useACF: true,
+            acfOptionPageIds: [],
+            auth: {},
+            verboseOutput: true,
+            perPage: 100,
+            concurrentRequests: 10,
+            includedRoutes: [
+                "/wp/v2/**",
+                "/wp-api-menus/**",
+                "/acf/v3/**",
+                // "**/*/*/categories",
+                // "**/*/*/posts",
+                // "**/*/*/pages",
+                // "**/*/*/media",
+                // "**/*/*/tags",
+                // "**/*/*/taxonomies",
+                // "**/*/*/users",
+            ],
+            excludedRoutes: ["/wp/v2/users/me", "/acf/v3/options", "/wp/v2/settings"],
+        },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
