133a68ebfc6a40efe339e75f8d88432c6a10863c
STUkh <stukak@gmail.com>
2019-03-26 16:09:59 +0200

    Save DB to local folder instead of Docker Volume

---
+++ src/plugins/gatsby-source-wordpress/normalize-base-url.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function normalizeBaseUrl(baseUrl) {
+  let normalized = baseUrl; // remove trailing slashes
+
+  normalized = normalized.replace(/\/+$/, ``); // remove protocol
+
+  normalized = normalized.replace(/^https?:\/\//, ``);
+  return normalized;
+}
+
+module.exports = normalizeBaseUrl;
